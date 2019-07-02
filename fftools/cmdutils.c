7d6d068963d615a6727be4aa26d4ddaf97b682af
keunho.yoo <keunho.yoo@gmail.com>
2019-07-02 14:00:48 +0900

    f

--- fftools/cmdutils.c
+++ fftools/cmdutils.c
@@ -1143,7 +1143,7 @@ static void print_program_info(int flags, int level)
 
     av_log(NULL, level, "%s version " FFMPEG_VERSION, program_name);
     if (flags & SHOW_COPYRIGHT)
-        av_log(NULL, level, " Copyright (c) %d-%d the FFmpeg developers",
+        av_log(NULL, level, " Copyright (c) %d-%d the FFmpeg developers!!",
                program_birth_year, CONFIG_THIS_YEAR);
     av_log(NULL, level, "\n");
     av_log(NULL, level, "%sbuilt with %s\n", indent, CC_IDENT);
